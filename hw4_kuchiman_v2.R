0dc384dd87e50926b583078c7d4e7cbfafb3b4a7
Nick Kuchimanchi <nitro41992@gmail.com>
2020-04-18 23:14:37 -0400

    Update hw4_kuchiman_v2.R

--- hw4_kuchiman_v2.R
+++ hw4_kuchiman_v2.R
@@ -16,13 +16,20 @@ setkey(members,"msno")
 merged_data = merge(train, members, by="msno")
 merged_data <- merged_data[,-(1),drop=FALSE]
 
+# Converting gender column to a scalar. Replacing blank rows with 0.5
 merged_data$gender <- revalue(merged_data$gender, c("female"=1))
 merged_data$gender <- revalue(merged_data$gender, c("male"=0))
 merged_data$gender[merged_data$gender == ""] = 0.5
+
+# Converting gender and is_churn into a factor
 merged_data$is_churn = as.factor(merged_data$is_churn)
 merged_data$gender = as.factor(merged_data$gender)
-merged_data$registration_init_time = as.numeric(Sys.Date() - as.Date(sprintf("%08d",merged_data$registration_init_time), origin="1970-01-01", format="%Y%m%d"))
 
+# Converting registration_init_time into time delta of today - registration_init_time
+merged_data$registration_init_time = as.numeric(
+  Sys.Date() - as.Date(sprintf("%08d",merged_data$registration_init_time),
+                       origin="1970-01-01",
+                       format="%Y%m%d"))
 
 # Get indicies
 y = merged_data[, 1]
@@ -36,6 +43,7 @@ test_inds = createDataPartition(y = 1:length(y), p = split_number, list = F)
 X_train = merged_data[-test_inds, ]
 X_test = merged_data[test_inds, ]
 
+# Fitting to the model
 model <- glm(is_churn ~ ., data = X_train, family = "binomial")
 
 
@@ -45,15 +53,18 @@ prob=predict(model,X_test,type="response")
 # Gathering predictions and calculating prediction error
 preds = round(prob)
 pred_error = mean(preds != X_test$is_churn)
-cat('The classification error is', pred_error, '\n')
+cat('The classification error is', pred_error,'\n')
 
+# Calculating ROC
 X_test$prob <- prob
-g <- roc(is_churn ~ prob, data = X_test)
+g <- roc(is_churn ~ prob, data = X_test, levels = c(0, 1), direction = "<")
 
 # Plotting ROC and calculating AUC
 plot(g, main='ROC')
 print(auc(g))
 
-# Training the model using cross_validation control
+# Training the model using cross_validation control and obtaining the prediction error.
+# Note: This error will be closer to the true classification error because it is an iterative test of the model using random sampling
+# of the train data.
 kf = cv.glm(data=X_train, glmfit=model, K=folds)
 cat('The cross-validation estimate of prediction error is', kf$delta[1])
